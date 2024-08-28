379415610e0f878cf13f29266ab3904218c142ad
Alexander <alexander@MBP-Alexander.fritz.box>
2024-08-28 22:13:23 +0300

    add user model

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+# from .views import UsersViewSet, AuthViewSet
+
+router_v1 = DefaultRouter()
+# router_v1.register('users', UsersViewSet, basename='users')
+# router_v1.register(r'auth', AuthViewSet, basename='auth')
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+# class UsersViewSet(viewsets.ModelViewSet):
+#     pass
+
+
+# class AuthViewSet(viewsets.GenericViewSet):
+#     pass
