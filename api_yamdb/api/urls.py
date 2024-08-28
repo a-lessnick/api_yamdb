94394de3795803a2038a7546f76316caae9a183d
Alexander <alexander@MBP-Alexander.fritz.box>
2024-08-28 23:15:01 +0300

    add user auth, base setup

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-# from .views import UsersViewSet, AuthViewSet
+from .views import AuthViewSet #UsersViewSet
 
 router_v1 = DefaultRouter()
 # router_v1.register('users', UsersViewSet, basename='users')
-# router_v1.register(r'auth', AuthViewSet, basename='auth')
+router_v1.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from datetime import timedelta
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reviews',
     'api',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -105,3 +108,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 AUTH_USER_MODEL = 'reviews.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
