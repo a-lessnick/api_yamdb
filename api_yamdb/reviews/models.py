82e9bb652ba5e14a31b8ec7e37d54901e1487bd2
Alexander <alexander@MBP-Alexander.fritz.box>
2024-08-29 22:25:36 +0300

    add permissions, fix user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class User(AbstractUser):
     REGEX_SIGNS = RegexValidator(r'^[\w.@+-]+\Z')
-
+    REGEX_ME = RegexValidator(r'[^m][^e]')
     NAME_MAX_LENGTH = 150
     EMAIL_MAX_LENGTH = 256
     ROLE_MAX_LENGTH = 64
@@ -23,7 +23,7 @@ class User(AbstractUser):
     username = models.CharField(
         unique=True,
         max_length=NAME_MAX_LENGTH,
-        validators=(REGEX_SIGNS,),
+        validators=(REGEX_SIGNS, REGEX_ME),
         verbose_name='Никнейм пользователя',
     )
     email = models.EmailField(
@@ -52,6 +52,14 @@ class User(AbstractUser):
         max_length=ROLE_MAX_LENGTH,
         verbose_name='Роль пользователя',
     )
+
+    @property
+    def is_admin(self):
+        return self.role == User.ADMIN or self.is_staff or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == User.MODERATOR
 
     class Meta:
         ordering = ('id',)
