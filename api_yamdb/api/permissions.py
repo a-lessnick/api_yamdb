92eb171a2955c2a67c4b282629122d27d2ed44ea
Andrey Starostin <161159835+a-lessnick@users.noreply.github.com>
2024-09-02 20:46:05 +0300

    YAMDB-12. Created views and serializers for Title, Category, Genre.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,39 +1,34 @@
 from rest_framework import permissions
 
 
-class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
-    """Проверка разрешений для админа, модератора и автора."""
-
-    def has_object_permission(self, request, view, obj):
+class AdminOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_admin
-            or request.user.is_moderator
-            or obj.author == request.user
+            request.user.is_authenticated
+            and request.user.is_admin
         )
 
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_admin
+        )
+
+
+class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
         )
 
-
-class IsAdmin(permissions.BasePermission):
-    """Проверка разрешений для админа."""
-
-    def has_permission(self, request, view):
-        """Переопределение разрешения."""
-        return request.user.is_authenticated and (
-            request.user.is_admin or request.user.is_superuser
-        )
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    """Проверка разрешений для админа или на чтение."""
-
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or (
-            request.user.is_authenticated
-            and (request.user.is_admin or request.user.is_superuser)
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
+            or request.user == obj.author
         )
