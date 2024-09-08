db201cfdd977c03895e4de22e948ee1eb367e2a0
Alexander <alexander@MBP-Alexander.fritz.box>
2024-09-08 20:14:11 +0300

    fix code

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,11 +10,8 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
 from rest_framework.views import APIView
 
-from rest_framework.exceptions import ValidationError
 from django.db import IntegrityError
 from rest_framework_simplejwt.views import TokenObtainPairView
 
