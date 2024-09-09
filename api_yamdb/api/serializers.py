581e06611d167a2bcf031452af44093582507d96
Alex Zheltov <lsswow69@gmail.com>
2024-09-09 20:49:10 +0300

    fixed review serializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -115,25 +115,13 @@ class ReviewSerializer(serializers.ModelSerializer):
         """Проверяет уникальность отзыва пользователя на одно произведение."""
         request = self.context.get('request')
         author = request.user
-        title_id = self.context.get('view').kwargs.get('title_id')
+        title = self.context.get('view').kwargs.get('title_id')
 
-        if request.method == 'POST':
-            if Review.objects.filter(
-                author=author, title_id=title_id
-            ).exists():
+        if self.instance is None:
+            if Review.objects.filter(title=title, author=author).exists():
                 raise serializers.ValidationError(
-                    'Нельзя оставить два отзыва на одно произведение.'
+                    "Вы уже оставляли отзыв на этот заголовок."
                 )
-        elif request.method in ['PATCH', 'PUT']:
-            if self.instance.author == author:
-                review_id = self.instance.id
-                existing_review = Review.objects.filter(
-                    author=author, title_id=title_id
-                ).exclude(id=review_id)
-                if existing_review.exists():
-                    raise serializers.ValidationError(
-                        'Нельзя оставить два отзыва на одно произведение.'
-                    )
 
         return data
 
